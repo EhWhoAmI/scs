5629888777e38f33cfd66818e3643b3bd117631c
Zyun <zyunlam@gmail.com>
2017-09-07 15:24:04 -0500

    Server now can connect. Did a lot of work in this commit. Cannot send repo, but it can send an empty repo.

--- src/scsserver/ScsServer.java
+++ src/scsserver/ScsServer.java
@@ -10,14 +10,25 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
@@ -35,39 +46,40 @@ import javax.swing.UnsupportedLookAndFeelException;
 //This is a bit of a mess, like always. Needs clearing up and documentation.
 /**
  * This starts a scs server
+ *
  * @author Lam Zyun
  */
-public class ScsServer extends JFrame{
+public class ScsServer extends JFrame {
 
     /**
      * Serial version uid
      */
     private static final long serialVersionUID = 1L;
-    
+
     /**
      * The file for the repo
      */
     private File repoFile;
-    
+
     /**
      * To check whether the server is running
      */
     boolean serverRunning = false;
-    
+
     /**
      * The settings of the server
      */
     private Properties settings = new Properties();
-    
+
     /**
      * The current instance
      */
     private static ScsServer currentInstance;
-    
+
     private static final int serverport = 19319; //SCS in numbers of the alphabet
     //Logger
     Logging logger = new Logging("/scsserver.log", true, false);
-        
+
     //For things.
     private JButton kill;
     private JButton info;
@@ -79,33 +91,37 @@ public class ScsServer extends JFrame{
     private JLabel servingRepo;
     private JLabel serverStatus;
     private JPanel pane;
-    
+
+    //The repo data
+    int repoCommitNumber;
+    String repoUUID;
+
     private ScsServer() {
         super("SCS server control panel");
         //Set up window
         setSize(600, 300);
-        
+
         //Window listeners
         addWindowListener(new windowlistener());
-        
+
         //Panel to store all the components
         pane = new JPanel();
         pane.setLayout(new GridLayout(3, 4, 10, 10)); //Set the layout
-        
+
         setLookAndFeel();
-        
+
         //Title
         title = new JLabel("SCS Server Control Panel");
         pane.add(title);
-        
+
         //The repo that we are serving
         servingRepo = new JLabel("Serving:");
         pane.add(servingRepo);
-        
+
         //The server status. Running, starting, not running
         serverStatus = new JLabel("Server status: Not running");
         pane.add(serverStatus);
-        
+
         //Button to start server
         startServerBttn = new JButton("Start Server");
         startServerBttn.setEnabled(false);
@@ -113,33 +129,33 @@ public class ScsServer extends JFrame{
             startServer();
         });
         pane.add(startServerBttn);
-        
+
         //Button to start server: Not implemented yet
         kill = new JButton("Kill Server");
         kill.setEnabled(false);
         pane.add(kill);
-        
+
         //Button to choose repo
         chooseRepo = new JButton("Choose Repo");
         chooseRepo.addActionListener((e) -> {
             chooseRepoDialog();
         });
         pane.add(chooseRepo);
-        
+
         //Config
         config = new JButton("SCS Server Config");
         config.addActionListener((e) -> {
-           configWin();
+            configWin();
         });
         pane.add(config);
-        
+
         //Log show
         logs = new JButton("Show Logs");
         logs.addActionListener((e) -> {
             logWin();
         });
         pane.add(logs);
-        
+
         //About. Nothing to show
         info = new JButton("About scs");
         info.addActionListener((e) -> {
@@ -147,16 +163,17 @@ public class ScsServer extends JFrame{
             JOptionPane.showMessageDialog(pane, "SCS\nVersion 0.0.0.0");
         });
         pane.add(info);
-        
+
         //Add all the panes
         add(pane);
-        
+
         setResizable(false);
         setVisible(true);
     }
-    
+
     /**
      * Check if a folder is a scs repo
+     *
      * @param fileCheck file to check
      * @return wether of not it is a scs repo
      */
@@ -166,30 +183,31 @@ public class ScsServer extends JFrame{
         File currentFile = new File(dbFile.getPath() + "/current");
         File UUIDFile = new File(dbFile.getPath() + "/UUID");
         File versionFile = new File(dbFile.getPath() + "/version");
-        
+
         return (dbFile.exists() & currentFile.exists() & UUIDFile.exists() & versionFile.exists());
     }
-    
+
     /**
      * SCS Server get instancs
+     *
      * @return a new instance of scs server
      */
-    public static ScsServer getInstance () {
-        if (currentInstance == null)
+    public static ScsServer getInstance() {
+        if (currentInstance == null) {
             currentInstance = new ScsServer();
+        }
         return currentInstance;
     }
-    
+
     /**
-     * Opens a window to show logs
-     * Could add: Logs refresh, clean log file.
+     * Opens a window to show logs Could add: Logs refresh, clean log file.
      */
     private void logWin() {
         try {
             //Open the file in a new window
             JFrame logWin = new JFrame("Scs Logs");
             logWin.setLayout(new BorderLayout());
-            
+
             JPanel logPanel = new JPanel();
             JTextArea logText = new JTextArea(5, 50);
             logText.setEditable(false);
@@ -204,7 +222,7 @@ public class ScsServer extends JFrame{
 
             logPanel.add(scrollPane);
             logWin.add(logPanel);
-            
+
             //Window preferences
             logWin.setPreferredSize(new Dimension(500, 500));
             logWin.setSize(500, 500);
@@ -214,12 +232,11 @@ public class ScsServer extends JFrame{
             Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /**
-     * Opens a config window for config.
-     * To continue work on.
+     * Opens a config window for config. To continue work on.
      */
-    private void configWin () {
+    private void configWin() {
         //Open config file
         File confFile = new File(System.getProperty("user.dir") + "/scsserver/conf.properties");
         try {
@@ -236,10 +253,11 @@ public class ScsServer extends JFrame{
 
                 //Add the default options
                 String filename = "";
-                if (repoFile != null) 
+                if (repoFile != null) {
                     filename = repoFile.getAbsolutePath();
+                }
                 settings.setProperty("repopath", filename);
-                
+
                 //Store settings
                 settings.store(new FileOutputStream(confFile), "NONE!!!");
             }
@@ -248,12 +266,12 @@ public class ScsServer extends JFrame{
 
             configWin.setAlwaysOnTop(true);
             configWin.setSize(300, 300);
-            
+
             JPanel winPanel = new JPanel();
             winPanel.setLayout(new GridLayout(3, 3));
             JLabel title = new JLabel("SCS Settings");
             winPanel.add(title);
-            
+
             {
                 //The file choose
                 JLabel fileChoose = new JLabel("File: ");
@@ -261,12 +279,12 @@ public class ScsServer extends JFrame{
             }
             configWin.add(winPanel);
             configWin.setVisible(true);
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             System.err.println("IO ERROR: " + ioe.getMessage());
             ioe.printStackTrace();
         }
     }
-    
+
     private void setLookAndFeel() {
         try {
             //Set look and feel
@@ -281,19 +299,19 @@ public class ScsServer extends JFrame{
             Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private void startServer () {
+
+    private void startServer() {
         logger.log("Starting server...");
         serverRunning = true;
         serverStatus.setText("Server status: Starting...");
         //Add server start code here
-        
+
         //When done,
         kill.setEnabled(true);
         startServerBttn.setEnabled(false);
     }
-    
-    private void chooseRepoDialog () {
+
+    private void chooseRepoDialog() {
         JFileChooser chooser = new JFileChooser();
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         chooser.setDialogTitle("Choose SCS repo");
@@ -308,18 +326,49 @@ public class ScsServer extends JFrame{
                 JOptionPane.showMessageDialog(pane, repoFile.getAbsolutePath() + "is not an scs repo");
                 repoFile = null;
             } else {
-                //Then pass
-                System.out.println(repoFile.toString() + " is scs repo.");
                 //Set properties
                 settings.setProperty("repopath", repoFile.getAbsolutePath());
                 //Edit label
                 servingRepo.setText("Serving: " + repoFile.getAbsolutePath());
                 startServerBttn.setEnabled(true);
+                //Get repo data
+                {
+                    File repoCommitFile = new File(repoFile.getAbsolutePath() + "/db/current");
+                    File repoUUIDFile = new File(repoFile.getAbsolutePath() + "/db/UUID");
+                    try {
+                        if (repoCommitFile.exists()) {
+                            Scanner repocireader = new Scanner(repoCommitFile);
+                            //Get int
+                            repoCommitNumber = repocireader.nextInt();
+                        }
+                        else {
+                            //Then kill the application
+                            throw new FileNotFoundException();
+                        }
+                        
+                        //Then get the uuid
+                        if (repoUUIDFile.exists()) {
+                            Scanner repoUUIDScanner = new Scanner (repoUUIDFile);
+                            
+                            //Get string
+                            repoUUID = repoUUIDScanner.nextLine();
+                        }
+                        else {
+                            throw new FileNotFoundException();
+                        }
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
+                        //Cannot find file
+                        JOptionPane.showMessageDialog(null, "Unable to open repo", "The format of the repo must not be correct. Please try again.", JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+
             }
         }
     }
-    
+
     class windowlistener implements WindowListener {
+
         @Override
         public void windowOpened(WindowEvent e) {
             //Open config file. If it exists, good, check it, if it doesn't, make it.
@@ -327,7 +376,7 @@ public class ScsServer extends JFrame{
             if (!configFile.exists()) {
                 try {
                     //Create parent dir. It will complain if I don't
-                    if(!configFile.getParentFile().exists()) {
+                    if (!configFile.getParentFile().exists()) {
                         configFile.getParentFile().mkdirs();
                     }
                     //Create file
@@ -338,18 +387,17 @@ public class ScsServer extends JFrame{
                 } catch (IOException ex) {
                     Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            }
-            else {
+            } else {
                 try {
                     //Read from config file
                     FileInputStream confin = new FileInputStream(configFile);
                     settings.load(confin);
                     confin.close();
-                    
+
                     //For presetting. Makes things easier
                     repoFile = new File(settings.getProperty("repopath"));
                     servingRepo.setText("Serving: " + repoFile.getAbsolutePath());
-                    
+
                     startServerBttn.setEnabled(true);
                     //Tests
                     settings.list(System.out);
@@ -373,10 +421,9 @@ public class ScsServer extends JFrame{
                 } catch (IOException ex) {
                     Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
+
                 System.exit(0);
-            }
-            else {
+            } else {
                 //Do nothing, just iconify.
                 setState(JFrame.ICONIFIED);
             }
@@ -407,9 +454,110 @@ public class ScsServer extends JFrame{
             //Chill
         }
     }
-    
+
+    private synchronized void haltServer() {
+        serverRunning = false;
+    }
+
+    private void runserver() {
+        try {
+            //Set up sockets (Non blocking)
+            ServerSocketChannel sockchannel = ServerSocketChannel.open();
+            sockchannel.configureBlocking(false);
+
+            //Set up host and port.
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(serverport);
+            ServerSocket serverSocket = sockchannel.socket();
+            serverSocket.bind(inetSocketAddress, SelectionKey.OP_ACCEPT);
+
+            //Create selector
+            Selector selector = Selector.open();
+            sockchannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            // Server loop
+            Runnable server = () -> {
+
+                while (serverRunning) {
+                    //The server connecyion
+                    Socket connection = null;
+                    try {
+                        selector.select();
+
+                        Set keys = selector.selectedKeys();
+                        Iterator it = keys.iterator();
+
+                        //Handle all kets
+                        while (it.hasNext()) {
+
+                            //Get key
+                            SelectionKey sk = (SelectionKey) it.next();
+
+                            it.remove();
+
+                            if (sk.isAcceptable()) {
+
+                                //Create socket channel
+                                ServerSocketChannel sschannel = (ServerSocketChannel) sk.channel();
+                                ServerSocket sock = sschannel.socket();
+                                connection = sock.accept();
+
+                                //Handle request
+                                connection.close();
+                            }
+                        }
+                    } catch (IOException ex) {
+                        Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
+                        logger.log("IO EXCEPTION!" + ex.getMessage());
+                        if (connection != null) {
+                            try {
+                                connection.close();
+                            } catch (IOException ex1) {
+                                Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex1);
+                            }
+                        };
+                    }
+                }
+            };
+        } catch (IOException ex) {
+
+            Logger.getLogger(ScsServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void handle(Socket connection) throws IOException {
+        //Set up input
+        InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+        BufferedReader br = new BufferedReader(isr);
+
+        //Set up output
+        PrintWriter writer = new PrintWriter(new BufferedOutputStream(connection.getOutputStream()));
+
+        //Get the data. The code to check whether it is a friend is 'c6711b33d73157f21d70ef7d1341e016e92f8443cedd7de866'
+        String friendStr = br.readLine();
+        if (!friendStr.equals("c6711b33d73157f21d70ef7d1341e016e92f8443cedd7de866")) {
+            //Deny. Might be hacker
+            return;
+        }
+        //Then, get the command he wants to process
+        String request = br.readLine();
+        if (request.startsWith("GET")) {
+            //Process get repo here
+            //Send repo data, and zip repo, and send repo.
+            //Send repo commit
+            writer.write(Integer.toString(repoCommitNumber));
+            writer.write(repoUUID);
+            if (repoCommitNumber == 0) {
+                //Don't send anything else. 
+                return;
+            }
+            //Then send data
+            
+        }
+    }
+
     /**
      * Main class of the scs server
+     *
      * @param args
      */
     public static void main(String[] args) {
